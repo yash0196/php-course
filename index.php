acf49a7e15e33de6d3ed437b0462fda0e3c922d8
Yashaswi Ranjan <yash@yash.one>
2020-10-16 14:52:33 +0530

    add variables

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <body>
     <?php
 	    $name = "PHP Store";
-		$credit = 1000;
+		$credit = 1000; 
 
 	    echo "<h1>Welcome to ".$name."!</h1>";
 	    echo "<h2>You have $".$credit." in your wallet.</h2>";
@@ -16,27 +16,27 @@
 	    $products['iPhone']=1000;
 	    $products['Toaster']=75;
 
+		$taxRate=0.0825;
+
+		function tax_calc($amount,$tax){
+	    	$addedTax = $amount*$tax;
+	    	$amountWithTax = round($amount+$addedTax,2);
+	    	return $amountWithTax;
+    	}
+
 	    foreach($products as $key => $value){
-		    echo "<p>The ".$key." costs ".$value."</p>";
+	    	$costWithTax = tax_calc($value,$taxRate);
+		    echo "<p>The ".$key." costs ".$costWithTax." with tax</p>";
 	    }
 
 	    echo "<h2>Items you can afford</h2>";
 
 	    foreach($products as $key => $value){
-		    if($value <= $credit ){
+	    	$costWithTax = tax_calc($value,$taxRate);
+		    if($costWithTax <= $credit ){
 		    	echo "<p>".$key."</p>"; 
 		    }
 	    }
-		$amount=800;
-		$taxRate=0.0825;
-		$addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
-		echo $addedTax;
-		function tax_calc($amount,$tax){
-			$calculate_tax = $amount*$tax;
-			$amount = round($amount+$calculate_tax,2);
-			return $amount;
-		}
-		echo tax_calc(750,0.223);
     ?>
   </body>
 </html>
